0d40816043830cfd6dd5d0d8801cea5345f19caa
Adrielle <ellekrau91@gmail.com>
2022-08-02 19:23:42 -0300

    Add basic kafka producer structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	godotenv.Load("../.env")
 }
 
-func newKafkaProducer() {
-
+func newKafkaProducer() (producer *kafka.Producer, err error) {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("BOOTSTRAP-SERVER"),
+	}
+	producer, err = kafka.NewProducer(configMap)
+	return
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ellekrau/fullcycle-kafka-go
 
 go 1.18
 
-require github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
