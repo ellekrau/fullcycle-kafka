f7765716b6a6b8fdff79162ac828547c3e307853
Adrielle <ellekrau91@gmail.com>
2022-08-02 23:34:20 -0300

    Add kafka message production using key to ensure that will always be consumed by the same consumer

--- cmd/main.go
+++ cmd/main.go
@@ -36,13 +36,19 @@ func main() {
 	//	log.Fatalln(fmt.Errorf("send kafka message error: %s", err.Error()))
 	//}
 
+	// Sends a kafka message using delivery channel and key to be consumed by the same consumer
+	err = produceKafkaMessage([]byte("1"), "Message with delivery channel and key!", os.Getenv("TOPIC"), producer, deliveryChannel)
+
+	// Async consume of delivery channel
 	go runKafkaMessageDeliveryReport(deliveryChannel)
 	producer.Flush(1000)
 }
 
 func newKafkaProducer() (producer *kafka.Producer, err error) {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": os.Getenv("BOOTSTRAP-SERVER"),
+		"bootstrap.servers":   os.Getenv("BOOTSTRAP-SERVER"),
+		"delivery.timeout.ms": "200",
+		"acks":                "-1",
 	}
 	producer, err = kafka.NewProducer(configMap)
 	return
